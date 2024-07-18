b93320b19d6ab94896729ac4ada55ef0e24486ed
YehorBoiar <boyaregor1@gmail.com>
2024-07-18 15:41:49 +0100

    Deleted UploadFile component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UpperBar from './components/UpperBar';
 import Signin from './pages/signin';
 import Register from './pages/register';
-import UploadFile from './components/UploadFile';
 import Home from './pages/home';
 import Dosound from './pages/dosound';
 
@@ -15,7 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dosound" element={<Dosound />} />
-            <Route path="/upload" element={<UploadFile />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/register" element={<Register />} />
           </Routes>
--- src/components/BookText.js
+++ src/components/BookText.js
@@ -4,7 +4,7 @@ import React from 'react';
 const BookText = ({ text }) => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[800px] h-[600px] p-4 border border-gray-300 rounded bg-white shadow-lg">
+      <div className="w-[800px] h-[600px] p-4 border rounded bg-white shadow-lg">
         <textarea
           className="w-full h-full p-2 border-none outline-none resize-none"
           value={text}

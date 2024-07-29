ca2a9e4b93dc1b32f82fb4a6c51c06b953984e6d
YehorBoiar <boyaregor1@gmail.com>
2024-07-29 10:08:56 +0100

    Refactored BookButton

--- src/components/BookButton.js
+++ src/components/BookButton.js
@@ -15,11 +15,29 @@ const BookButton = ({ book, onClick }) => {
     }
   }, [book.path]);
 
+  const handleClick = () => {
+    onClick(book.path);
+  };
+
+  const renderImage = () => {
+    if (imageSrc) {
+      return <img src={imageSrc} alt="Book cover" className="w-24 h-32 mr-2 rounded-md" />;
+    }
+    return null;
+  };
+
+  const renderTitle = () => {
+    if (book.metadata && book.metadata["/Title"]) {
+      return `${book.metadata["/Title"]} by ${book.metadata["/Author"]}`;
+    }
+    return "Unknown Book";
+  };
+
   return (
     <div className="mb-2 bg-gray-200 flex flex-col items-center p-2 rounded-md">
-      <button onClick={() => onClick(book.path)} className="text-black hover:underline justify-center items-center flex flex-col">
-        {imageSrc && <img src={imageSrc} alt="Book cover" className="w-24 h-32 mr-2 rounded-md" />}
-        {book.metadata && book.metadata["/Title"] ? `${book.metadata["/Title"]} by ${book.metadata["/Author"]}` : "Unknown Book"}
+      <button onClick={handleClick} className="text-black hover:underline justify-center items-center flex flex-col">
+        {renderImage()}
+        {renderTitle()}
       </button>
     </div>
   );

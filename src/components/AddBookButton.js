aad34e06f276f602fcc7ae506fa1f8b41faf0f88
YehorBoiar <boyaregor1@gmail.com>
2024-08-05 11:36:13 +0100

    Fixed token retrieving

--- src/components/AddBookButton.js
+++ src/components/AddBookButton.js
@@ -12,15 +12,27 @@ const AddBookButton = ({ onBookAdded }) => {
       alert("Please select a file first!");
       return;
     }
+
     const backendUrl = process.env.REACT_APP_BACKEND_URL;
     const formData = new FormData();
     formData.append('pdf_file', file);
-    const cookie = document.cookie.split(';').find(cookie => cookie.startsWith('token')).split('=')[1];
+    const tokenCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('token='));
+    
+    let token;
+    if (tokenCookie) {
+      token = tokenCookie.split('=')[1];
+      console.log('Token:', token);
+    } else {
+      console.error('Token cookie not found');
+      alert('Token cookie not found. Please login.');
+      return;
+    }
+
     try {
       await axios.post(`${backendUrl}/add_book`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
-          'Authorization': `Bearer ${cookie}`
+          'Authorization': `Bearer ${token}`
         }
       });
 
